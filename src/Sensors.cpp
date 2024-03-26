8dc8dc870327ab9677c873fb69ec12ac79d4c2b3
Mike Wilson <39413591+MikeTheGent@users.noreply.github.com>
2024-03-26 16:48:53 +0000

    Add set resolution to sensor.

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -18,6 +18,7 @@ namespace Sensors {
 
     bool begin() {
         hdc1080.begin(0x40);
+        hdc1080.setResolution(HDC1080_RESOLUTION_14BIT, HDC1080_RESOLUTION_14BIT);
         return bh1750.begin(BH1750_TO_GROUND);
     }
 
